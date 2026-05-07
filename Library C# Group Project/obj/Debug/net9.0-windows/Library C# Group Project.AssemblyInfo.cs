6bbf7408973567523367e13ef41f6e332ab0a2e2
HolyRatgamer <iggy.persson1@gmail.com>
2026-05-07 22:35:14 +0200

    Start of  GUI
    Started the work of the GUI, some testing parts are still inside due to being W.I.P. Had errors during testing and added assembly name into the CSPROJ, uncertain if this will affect anything else.

--- Library C# Group Project/obj/Debug/net9.0-windows/Library C# Group Project.AssemblyInfo.cs
+++ Library C# Group Project/obj/Debug/net9.0-windows/Library C# Group Project.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Library C# Group Project")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Library_C_Group_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351645fdbcdce58e4ef54baf424f45222b06b03c")]
-[assembly: System.Reflection.AssemblyProductAttribute("Library C# Group Project")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Library C# Group Project")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8179778a7957a105ea5486f1460af56071710cb1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Library_C_Group_Project")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Library_C_Group_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
