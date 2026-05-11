39623a06646b1a03e58e4907c6ddb49cceed85b1
BirdiePhoenix <123377512+BirdiePhoenix@users.noreply.github.com>
2026-05-11 11:41:51 +0200

    added so loanDate is set

--- Library C# Group Project/Customer.cs
+++ Library C# Group Project/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,23 +11,28 @@ namespace Library_C__Group_Project
     {
         public string Name { get; set; }
         public string CustomerID { get; set; }
+        public int LateFee { get; set; }
         public List<Book> LoanedBooks { get; set; } = new List<Book>();
 
         public Customer (string name, string customerID)
         {
             Name = name;
             CustomerID = customerID;
+            LateFee = 0;
         }
 
         public void AddLoan(Book book)
         {
+            book.LoanDate = DateTime.Now;
             LoanedBooks.Add(book);
         }
 
         public void RemoveLoan(Book book)
         {
+            book.LoanDate = null;
             LoanedBooks.Remove(book);
         }
+
 
         public void GetInfo()
         {
